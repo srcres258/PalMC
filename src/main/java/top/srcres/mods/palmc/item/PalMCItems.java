8e450b5e8cb370a3e3b766707d60865847771dd8
src_resources <src.res.211@gmail.com>
2024-03-14 22:54:01 +0800

    Remove test item
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/main/java/top/srcres/mods/palmc/item/PalMCItems.java
+++ src/main/java/top/srcres/mods/palmc/item/PalMCItems.java
@@ -1,6 +1,5 @@
 package top.srcres.mods.palmc.item;
 
-import net.minecraft.core.Registry;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -11,12 +10,10 @@ import java.util.function.Supplier;
 
 public class PalMCItems {
     public static DeferredHolder<Item, SpawnEggItem> HORUS_SPAWN_EGG;
-    public static DeferredHolder<Item, Item> TEST;
 
     // Has to be called manually to ensure the loading order is correct
     // to shed unexpected loading exceptions.
     public static void init() {
-        TEST = register("test", () -> new Item(new Item.Properties().stacksTo(64).setNoRepair()));
         HORUS_SPAWN_EGG = register("horus_spawn_egg", () -> new SpawnEggItem(PalMCEntities.HORUS.get(), 10592673, 16711680, new Item.Properties()));
     }
 
